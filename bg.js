9bc0132ab2be2376acfe8e5db32777e42e4fb2dc
tophf <tophf@gmx.com>
2019-08-30 20:39:19 +0300

    impersonate xhr as the URL's origin

--- bg.js
+++ bg.js
@@ -1,4 +1,4 @@
-﻿'use strict';
+'use strict';
 
 chrome.tabs.onActivated.addListener(({tabId}) => {
   contentScriptInit(tabId);
@@ -57,7 +57,10 @@ function contentScriptInit(tabId, frameId) {
   }, ignoreLastError);
 }
 
-function fetchInfo(src, tabId, frameId) {
+async function fetchInfo(src, tabId, frameId) {
+  await new Promise(resolve =>
+    chrome.permissions.request({permissions: ['webRequest', 'webRequestBlocking']}, resolve));
+  const spoofer = chrome.webRequest && spoofReferer(src);
   const xhr = new XMLHttpRequest();
   try {
     xhr.open('HEAD', /^https?:\/\//.test(src) ? src : '///throw');
@@ -67,18 +70,41 @@ function fetchInfo(src, tabId, frameId) {
   xhr.timeout = 10e3;
   xhr.ontimeout = xhr.onerror = xhr.onreadystatechange = e => {
     const info = {};
-    if (xhr.readyState === XMLHttpRequest.HEADERS_RECEIVED) {
+    if (xhr.status >= 300 || e.type === 'timeout' || e.type === 'error') {
+      info.error = true;
+    } else if (xhr.readyState === XMLHttpRequest.HEADERS_RECEIVED) {
       info.size = xhr.getResponseHeader('Content-Length') | 0;
       info.type = xhr.getResponseHeader('Content-Type');
-    } else if (xhr.status >= 300 || e.type === 'timeout' || e.type === 'error') {
-      info.error = true;
     } else {
       return;
     }
     chrome.tabs.sendMessage(tabId, info, {frameId}, ignoreLastError);
+    if (spoofer)
+      chrome.webRequest.onBeforeSendHeaders.removeListener(spoofer);
   };
   xhr.send();
-  return true;
+}
+
+function spoofReferer(src) {
+  const spoofer = ({requestHeaders}) => {
+    let ref = requestHeaders.find(h => h.name.toLowerCase() === 'referer');
+    if (!ref)
+      requestHeaders.push((ref = {name: 'Referer'}));
+    ref.value = new URL(src).origin + '/';
+    return {requestHeaders};
+  };
+  const filter = {
+    tabId: -1,
+    types: ['xmlhttprequest'],
+    urls: [src.split('#', 1)[0] + '*'],
+  };
+  const extras = [
+    'requestHeaders',
+    'blocking',
+    chrome.webRequest.OnBeforeSendHeadersOptions.EXTRA_HEADERS,
+  ].filter(Boolean);
+  chrome.webRequest.onBeforeSendHeaders.addListener(spoofer, filter, extras);
+  return spoofer;
 }
 
 function ignoreLastError() {
