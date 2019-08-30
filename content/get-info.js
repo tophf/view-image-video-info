0cf453268c3f7d356ce7c0ccada68c742a233848
tophf <tophf@gmx.com>
2019-08-30 20:42:57 +0300

    find a better match for img/video inside a link + refactor

--- content/get-info.js
+++ content/get-info.js
@@ -1,22 +1,21 @@
 'use strict';
 
-if (!window.hasOwnProperty(Symbol.for('info'))) {
+!window.hasOwnProperty(Symbol.for('info')) && (() => {
   const key = Symbol.for('info');
   for (const el of document.getElementsByClassName(chrome.runtime.id))
     el.remove();
-  window.addEventListener('contextmenu', function onMenu(e) {
-    try {
-      chrome.i18n.getUILanguage();
-    } catch (e) {
-      // we've been orphaned
-      delete window[key];
-      window.removeEventListener('contextmenu', onMenu);
-      return;
+  window.addEventListener('contextmenu', onMenu, {passive: true});
+
+  function onMenu(e) {
+    if (!quitWhenOrphaned()) {
+      const el = e.composedPath()[0].closest('img, video, a');
+      const img = !el || el.tagName !== 'A' ? el :
+        [...el.querySelectorAll('img, video')].find(hitTest, e);
+      setInfo(img);
     }
-    const el = e.composedPath()[0].closest('img, video, a');
-    const img = !el || el.tagName !== 'A' ? el :
-      document.elementsFromPoint(e.clientX, e.clientY)
-        .find(el => el.tagName === 'IMG' || el.tagName === 'VIDEO');
+  }
+
+  function setInfo(img) {
     window[key] = img && {
       img,
       src: img.src || img.currentSrc,
@@ -27,5 +26,21 @@ if (!window.hasOwnProperty(Symbol.for('info'))) {
       w: img.naturalWidth || img.videoWidth,
       h: img.naturalHeight || img.videoHeight,
     };
-  }, {passive: true});
-}
+  }
+
+  function hitTest(el) {
+    const b = el.getBoundingClientRect();
+    return b.left <= this.clientX && b.top <= this.clientY &&
+           b.right >= this.clientX && b.bottom >= this.clientY;
+  }
+
+  function quitWhenOrphaned() {
+    try {
+      chrome.i18n.getUILanguage();
+    } catch (e) {
+      delete window[key];
+      window.removeEventListener('contextmenu', onMenu);
+      return true;
+    }
+  }
+})();
