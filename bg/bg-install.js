c414c4881abee4b684f915044d035f4c2bd202f8
tophf <tophf@gmx.com>
2019-09-07 13:57:04 +0300

    use targetUrlPatterns to show the link menu

--- bg/bg-install.js
+++ bg/bg-install.js
@@ -3,23 +3,39 @@ import {contentScriptInit, ignoreLastError} from './bg.js';
 chrome.runtime.onInstalled.addListener(() => {
   localStorage.naviTargets = '';
   contentScriptInit(null);
+
   const opts = {
     type: 'normal',
     title: chrome.i18n.getMessage('contextMenu'),
   };
+
   chrome.contextMenus.create({
     ...opts,
     id: 'info',
     contexts: ['image', 'video'],
     documentUrlPatterns: ['*://*/*', 'file://*/*'],
   }, ignoreLastError);
-  chrome.contextMenus.create({
-    ...opts,
-    id: 'link',
-    contexts: ['link'],
-    documentUrlPatterns: [
-      'https://imgur.com/*',
-      'https://www.facebook.com/*',
-    ],
-  }, ignoreLastError);
+
+  for (const [id, {pages, links}] of Object.entries({
+    'imgur.com': {
+      pages: ['/', '/t/*'],
+      links: ['/gallery/*', '/t/*'],
+    },
+    '*.facebook.com': {
+      pages: ['/*'],
+      links: ['/*/photos/*?type=*'],
+    },
+  })) {
+    chrome.contextMenus.create({
+      ...opts,
+      id: 'link:' + id,
+      contexts: ['link'],
+      documentUrlPatterns: pages.map(expandUrl, id),
+      targetUrlPatterns: links.map(expandUrl, id),
+    }, ignoreLastError);
+  }
+
+  function expandUrl(s) {
+    return s.includes('://') ? s : `*://${this}${s}`;
+  }
 });
